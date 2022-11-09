05a7c30fb8d5f571e459000c449e51737ec916e8
ORomig <oliviaromig7@gmail.com>
2022-11-08 18:01:37 -0600

    working on it

---
+++ exercise01/multTable.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Multiplication Table!</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <h1>Multiplication Table!</h1>
+  <?php
+    Echo "<table>
+    <tr>
+      <th></th>
+      <th></th>
+    </tr>
+    
+    <tr>
+    <th>1 </th>
+    <th>2 </th>
+    </tr>
+   
+    <tr>
+    <td></td>
+      <td></td>
+      </tr>
+  </table>"
+    ?>
+  
+
+</body>
+
+</html>
