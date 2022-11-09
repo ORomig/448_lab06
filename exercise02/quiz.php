ba8d54fefc39a39d79eb43fedca33d64f3e18e5f
ORomig <oliviaromig7@gmail.com>
2022-11-09 08:54:05 -0600

    started exercise02

---
+++ exercise02/quiz.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>Multiplication Table!</title>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <h1>Multiplication Table!</h1>
+  <p>Multiplication Table for 100x100</p>
+</body>
+</html>
